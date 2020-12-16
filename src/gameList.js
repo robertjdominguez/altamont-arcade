9ef177f375507f7a76bd3bab7a92af85f810934c
Rob Dominguez <rtjdominguez@gmail.com>
2020-12-16 10:01:09 -0600

    Update gameList.js

--- src/gameList.js
+++ src/gameList.js
@@ -1,18 +1,8 @@
 // Array of students' games
 export const games = [
     {
-      title: `Ballard's Birds`,
-      author: `Dominguez`,
-      description: `Ballard's Birds is a platformer game that puts you in control of the fearless Mr. Ballard. Help him destroy flocks of birds, grab some jambalaya, and rescue sweet Rosie.`,
-      link: `https://makecode.com/_YWXdPbRmMCyj`,
-      image: `images/bbgif.gif`,
-      protagonist: `Mr. Ballard`,
-      genre: `Platformer`,
-      term: `Fall 2020`,
-    },
-    {
       title: `Turkey Trot`,
-      author: `Bennettt M.`,
+      author: `Bennett M.`,
       description: `Help the turkey find his way through the maze and past the obstacles. Before you know it, he'll be gobbling for victory at the end of the map!`,
       link: `https://makecode.com/_CALCuVach2Uz`,
       image: `images/gobbles.gif`,
@@ -52,7 +42,7 @@ export const games = [
     },
     {
       title: `Airships Galore`,
-      author: `Marko Maglic-Kinnunen`,
+      author: `Marko M-K`,
       description: `Airships Galore is a platformer game based om the airship levels from Super Mario. The game's protagonist name is Ludwig and he is fighting his entire Airshipp fleet just to get better wages.`,
       link: `https://makecode.com/_15W3MXLzoCKj`,
       image: `images/download.gif`,
@@ -78,6 +68,16 @@ export const games = [
       image: `images/vfb.gif`,
       protagonist: `Valkyrie`,
       genre: `Adventure`,
+      term: `Fall 2020`,
+    },
+    {
+      title: `Ballard's Birds`,
+      author: `Dominguez`,
+      description: `Ballard's Birds is a platformer game that puts you in control of the fearless Mr. Ballard. Help him destroy flocks of birds, grab some jambalaya, and rescue sweet Rosie.`,
+      link: `https://makecode.com/_YWXdPbRmMCyj`,
+      image: `images/bbgif.gif`,
+      protagonist: `Mr. Ballard`,
+      genre: `Platformer`,
       term: `Fall 2020`,
     },
   ];
